2ca28612b7e03d5d581ed41ca966780035146c89
Ahmad Sofyan <ahmadpie69@gmail.com>
2024-01-10 22:44:41 +0700

    add instance web-server

--- module/gcp/instances/vars.tf
+++ module/gcp/instances/vars.tf
@@ -31,6 +31,7 @@ variable "machine_type" {
   description = "your machine type of instance e.g e2-medium"
 }
 
+# etc
 variable "tags" {
   type        = list(string)
   description = "your network tags, should be array of string"
@@ -38,16 +39,19 @@ variable "tags" {
 
 variable "image" {
   type        = string
+  default     = "ubuntu-os-cloud/ubuntu-2204-jammy-v20231213a"
   description = "your image of instance e.g ubuntu-os-cloud/ubuntu-2204-jammy-v20231213a"
 }
 
 variable "disk_type" {
   type        = string
+  default     = "pd-ssd"
   description = "type of your disk e.g pd-ssd stand for persistent ssd"
 }
 
 variable "disk_size" {
   type        = number
+  default     = 10
   description = "size of your disk in GB, should be number, default is 10"
 }
 
@@ -63,5 +67,6 @@ variable "subnetwork" {
 
 variable "metadata_startup_script" {
   type        = string
+  default     = "sudo apt update -y && sudo apt upgrade -y"
   description = "startup script of your instance, should be string, you also can use <<EOF EOF>> tag for multi string"
 }
