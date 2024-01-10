2a1dd76ed94dfc263e1454966db8660c555be914
Ahmad Sofyan <ahmadpie69@gmail.com>
2024-01-10 22:19:20 +0700

    created vm in production

--- module/gcp/instances/output.tf
+++ module/gcp/instances/output.tf
@@ -1,9 +1,9 @@
 # display private and public IP of instance created
 
-output "ip-private-vm-default" {
+output "ip-private-default" {
   value = google_compute_instance.default.network_interface[*].network_ip
 }
 
-output "ip-public-vm-default" {
+output "ip-public-default" {
   value = google_compute_instance.default.network_interface[*].access_config[*].nat_ip
 }
